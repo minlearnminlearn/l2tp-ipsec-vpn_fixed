8894d01ab2d9f3240984dc41220d9a3cd47e894f
minlearnminlearn <minlearn@minlearn.org>
2024-05-22 23:57:39 +0800

    111

--- vpn.sh
+++ vpn.sh
@@ -12,12 +12,12 @@ cf_ipsec="/etc/ipsec.conf"
 cf_ipsec_secrets="/etc/ipsec.secrets"
 cf_sysctl="/etc/sysctl.conf"
 
-cfurl_ppp_secrets="https://raw.githubusercontent.com/iTeeLion/l2tp-ipsec-vpn/master/configs/etc/ppp/chap-secrets"
-cfurl_l2tp="https://raw.githubusercontent.com/iTeeLion/l2tp-ipsec-vpn/master/configs/etc/xl2tpd/xl2tpd.conf"
-cfurl_l2tp_options="https://raw.githubusercontent.com/iTeeLion/l2tp-ipsec-vpn/master/configs/etc/ppp/options.xl2tpd"
-cfurl_ipsec="https://raw.githubusercontent.com/iTeeLion/l2tp-ipsec-vpn/master/configs/etc/ipsec.conf"
-cfurl_ipsec_secrets="https://raw.githubusercontent.com/iTeeLion/l2tp-ipsec-vpn/master/configs/etc/ipsec.secrets"
-cfurl_sysctl="https://raw.githubusercontent.com/iTeeLion/l2tp-ipsec-vpn/master/configs/etc/sysctl.conf"
+cfurl_ppp_secrets="https://raw.githubusercontent.com/minlearnminlearn/l2tp-ipsec-vpn_fixed/master/configs/etc/ppp/chap-secrets"
+cfurl_l2tp="https://raw.githubusercontent.com/minlearnminlearn/l2tp-ipsec-vpn_fixed/master/configs/etc/xl2tpd/xl2tpd.conf"
+cfurl_l2tp_options="https://raw.githubusercontent.com/minlearnminlearn/l2tp-ipsec-vpn_fixed/master/configs/etc/ppp/options.xl2tpd"
+cfurl_ipsec="https://raw.githubusercontent.com/minlearnminlearn/l2tp-ipsec-vpn_fixed/master/configs/etc/ipsec.conf"
+cfurl_ipsec_secrets="https://raw.githubusercontent.com/minlearnminlearn/l2tp-ipsec-vpn_fixed/master/configs/etc/ipsec.secrets"
+cfurl_sysctl="https://raw.githubusercontent.com/minlearnminlearn/l2tp-ipsec-vpn_fixed/master/configs/etc/sysctl.conf"
 
 msg_cmd_control="$0 control start|stop|restart|status"
 msg_cmd_editconfig="$0 edit-config secrets|l2tp|l2tp-options|ipsec|ipsec-secrets|sysctl"
@@ -25,7 +25,7 @@ msg_cmd_installconfigs="$0 install-config all|secrets|l2tp|l2tp-options|ipsec|ip
 msg_cmd_server="$0 server install|reinstall|remove|purge"
 
 function showAreYouShure() {
-    read -p "Are you sure? [y/n] " -n 1 -r
+    read -p "Are you sure? [y/n] " -n 1 -r </dev/tty
     echo ""
     if [[ ! $REPLY =~ ^[Yy]$ ]]
     then
@@ -36,30 +36,39 @@ function showAreYouShure() {
 function doControl(){
     case $1 in
         start)
-	    echo "Trying to start $ipsec & $l2tp"
-	    eval "systemctl start $ipsec"
-	    eval "systemctl start $l2tp"
+	    echo "Trying to start ${ipsec}-starter & $l2tp"
+	    eval "systemctl start ${ipsec}-starter $l2tp"
 	;;
 	stop)
-	    echo "Trying to stop $ipsec & $l2tp"
-	    eval "systemctl stop $ipsec"
-	    eval "systemctl stop $l2tp"
+	    echo "Trying to stop ${ipsec}-starter & $l2tp"
+	    eval "systemctl stop ${ipsec}-starter $l2tp"
 	;;
 	restart)
-	    echo "Trying to restart $ipsec & $l2tp"
-	    eval "systemctl restart $ipsec"
-	    eval "systemctl restart $l2tp"
+	    echo "Trying to restart ${ipsec}-starter & $l2tp"
+	    eval "systemctl restart ${ipsec}-starter $l2tp"
 	;;
 	status)
 	    echo "Services status:"
-	    eval "systemctl status $ipsec"
-	    eval "systemctl status $l2tp"
+	    eval "systemctl status ${ipsec}-starter $l2tp"
 	;;
 	*)
 	    echo "Usage: $msg_cmd_control"
 	;;
     esac
 }
+
+
+function clearFirewall(){
+    echo "clearing firewall"
+    iptables -P INPUT ACCEPT
+    iptables -P FORWARD ACCEPT
+    iptables -P OUTPUT ACCEPT
+    iptables -t nat -F
+    iptables -t mangle -F
+    iptables -F
+    iptables -X
+}
+
 
 function setupFirewall(){
     echo "Allow nat and masquerading in firewall"
@@ -77,10 +86,33 @@ function allowIpForward(){
 }
 
 function doSetupNetwork() {
+    clearFirewall
     # ToDo
     # uncomment row "net.ipv4.ip_forward = 1" in /etc/sysctl.conf
     setupFirewall
     allowIpForward
+}
+
+function doAddStandip() {
+
+  echo "switching to multiple ip and multiple process mode"
+  eval "systemctl stop $l2tp"
+  eval "systemctl disable $l2tp"
+  xl2tpd -c /etc/xl2tpd/xl2tpd.conf -p /var/run/xl2tpd.pid
+
+  lastfilenum=`echo $(find /etc/xl2tpd/xl2tpd*|while read LINE; do echo ${LINE%%.conf}|grep -Eo '[0-9]+$';done|sort -r|head -n1)`;
+  [[ $lastfilenum == '' ]] && lastfilenum=0;
+  lastfilenumadded=`expr $lastfilenum + 1`;
+  wget -O /etc/xl2tpd/xl2tpd"$lastfilenumadded".conf "$cfurl_l2tp"
+  read -p "give a newip:" myIP </dev/tty
+  sed -e "s/yourip/$myIP/g" -e "s/rangestart/$lastfilenumadded/g" -i /etc/xl2tpd/xl2tpd"$lastfilenumadded".conf
+  xl2tpd -c /etc/xl2tpd/xl2tpd"$lastfilenumadded".conf -p /var/run/xl2tpd"$lastfilenumadded".pid
+
+  clearFirewall
+  iptables -t nat -A POSTROUTING -s 10.19.0.0/24 -o eth0 -j SNAT --to-source `echo $(ip a s|sed -ne '/127.0.0.1/!{s/^[ \t]*inet[ \t]*\([0-9.]\+\)\/.*$/\1/p}'|head -n1)`
+  iptables -t nat -A POSTROUTING -s 10.19."$lastfilenumadded".0/24 -o eth0 -j SNAT --to-source $myIP
+
+  #eval "systemctl restart ${ipsec}-starter"
 }
 
 function doEditConfig(){
@@ -161,7 +193,7 @@ function doInstallConfig(){
 
 function askInstallConfigs(){
     while true; do
-	read -p "Do you want to install preconfigured config files? [y/n] " yn
+	read -p "Do you want to install preconfigured config files? [y/n] " yn </dev/tty
 	case $yn in
 	    [Yy]* ) 
 		doInstallConfigAll 
@@ -180,8 +212,17 @@ function askInstallConfigs(){
 function doServer(){
     case $1 in
 	install)
+            doSetupNetwork
 	    eval "$PM install -y $ipsec $l2tp"
-	    askInstallConfigs
+	    doInstallConfigAll
+            myIP=$(ip a s|sed -ne '/127.0.0.1/!{s/^[ \t]*inet[ \t]*\([0-9.]\+\)\/.*$/\1/p}'|head -n1)
+            sed -e "s/yourip/$myIP/g" -e "s/rangestart/0/g" -i "$cf_l2tp"
+            read -p "give a user:" myUser </dev/tty
+            read -p "give a pass:" myPass </dev/tty
+            sed -e "s/testvpnuser/$myUser/g" -e "s/testvpnpassword/$myPass/g" -i "$cf_ppp_secrets"
+            read -p "give a sharedpsk:" myPSK </dev/tty
+            sed -i "s/PUT_YOUR_PSK_HERE/$myPSK/g" "$cf_ipsec_secrets"
+            doControl restart
 	;;
 	reinstall)
 	    eval "$PM reinstall -y $ipsec $l2tp"
@@ -194,6 +235,7 @@ function doServer(){
 	purge)
 	    showAreYouShure
 	    eval "$PM purge -y $ipsec $l2tp"
+            rm -rf /etc/xl2tpd/*
 	;;
 	*)
 	    echo "Usage: $msg_cmd_server"
@@ -216,6 +258,9 @@ function showHelp(){
     echo "- setup network (allow nat and masquerading in firewall and allow ip forwarding)"
     echo "	$0 setup-network" 
     echo " "
+    echo "- Add standip (switch mode and add multiple standalone ip)"
+    echo "	$0 add-standip" 
+    echo " "
     echo "- install/remove server (Packages: $ipsec & $l2tp)"
     echo "	$msg_cmd_server" 
     echo " "
@@ -227,6 +272,9 @@ case $1 in
     ;;
     setup-network)
 	doSetupNetwork
+    ;;
+    add-standip)
+	doAddStandip
     ;;
     edit-config)
 	doEditConfig $2
